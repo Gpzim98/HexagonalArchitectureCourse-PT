b1b61d529228ab0cb97b4be65dfad1028be95120
gpzim98 <gpzim98@gmail.com>
2022-03-20 17:04:54 +0000

    Criando a estate machine

--- BookingService/Core/Domain/Entities/Booking.cs
+++ BookingService/Core/Domain/Entities/Booking.cs
@@ -1,9 +1,5 @@
 ﻿using Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Action = Domain.Enums.Action;
 
 namespace Domain.Entities
 {
@@ -15,5 +11,18 @@ namespace Domain.Entities
         public DateTime End { get; set; }
         private Status Status { get; set; }
         public Status CurrentStatus { get { return this.Status; } }
+
+        public void ChangeState(Action action)
+        {
+            this.Status = (this.Status, action) switch
+            {
+                (Status.Created,  Action.Pay)     => Status.Paid,
+                (Status.Created,  Action.Cancel)  => Status.Canceled,
+                (Status.Paid,     Action.Finish)  => Status.Finished,
+                (Status.Paid,     Action.Refound) => Status.Refounded,
+                (Status.Canceled, Action.Reopen)  => Status.Created,
+                _ => this.Status
+            };
+        }
     }
 }
---
+++ BookingService/Core/Domain/Enums/Action.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Enums
+{
+    public enum Action
+    {
+        Pay = 0,
+        Finish = 1,  // after paid and used
+        Cancel = 2,  // can never be paid 
+        Refound = 3, // Paid then refound
+        Reopen = 4,  // canceled 
+    }
+}
